352fb63c37619fa215f19515ba6f2912a8b155ff
Sateesh <sateeshnaidu900@gmail.com>
2021-03-10 14:22:50 +0530

    Added insert and alter table queries in SQL

--- SQL-Queries.sql
+++ SQL-Queries.sql
@@ -7,6 +7,62 @@ SQL can set permissions on tables, procedures, and views
 -- CREATE DATABASE database_name;  
 CREATE DATABASE Customers;
 
+-- SQL CREATE TABLE Statement
+
+/* Syntax for create table
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+*/
+create table person(
+    name varchar(255),
+    age int,
+    dob date,
+    address varchar(255),
+    salary float
+);
+
+-- show tablespoon
+show tables;
+
+-- SQL INSERT INTO Table Syntax 
+-- 1. Specify both the column names and the values to be inserted:
+
+/* 
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+*/
+
+insert into person(name, age,dob,address) values("sateesh",30,1990-08-14,"AndhraPradesh");
+
+-- SQL ALTER TABLE Statement
+
+-- ALTER TABLE - ADD Column Syntax
+/*
+ALTER TABLE table_name
+ADD column_name datatype;
+*/
+
+alter table person add email varchar(30);
+
+-- ALTER TABLE - DROP COLUMN Syntax
+
+/* 
+ALTER TABLE table_name
+DROP COLUMN column_name;
+*/
+
+alter table person drop column email;
+
+
+ALTER TABLE person RENAME COLUMN dateofbirth TO dob;
+-- check for changes are successfull/not
+desc person;
+
+
 /* 
 Make sure you have admin privilege before dropping any database.
 DROP DATABASE databasename;
@@ -27,22 +83,5 @@ TO DISK = 'filepath';
 BACKUP DATABASE Customers TO DISK = 'D:\backups\CustomersDB.bak';
 
 
-/* 
-Create a Table
 
-CREATE TABLE table_name (
-    column1 datatype,
-    column2 datatype,
-    column3 datatype,
-   ....
-);
 
-*/
-
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
